513237a948195419167491adeeb8f6ae5110c108
Blake Ehlinger <blake_ehlinger1@baylor.edu>
2022-03-28 19:13:08 -0500

    revisions

--- emails.py
+++ emails.py
@@ -4,6 +4,16 @@ from twilio.rest import Client
 outlook = win32com.client.Dispatch("Outlook.Application")
 outlook_ns = outlook.GetNamespace("MAPI")
 
+accountSID = 'AC5cc40e15690b8315dfd06072069f8269'
+
+authToken = 'f1035dbbff19081db65e6bb8821890ca'
+
+client = Client(accountSID,authToken)
+
+TwilioNumber = "+19125206973"
+
+mycellphone = "+18326923524"
+
 
 
 myfolder = outlook_ns.Folders['blake_ehlinger1@baylor.edu'].Folders['Inbox']
@@ -13,11 +23,14 @@ messages = myfolder.Items
 messagecount = 0
 
 for message in messages:
-    messagecount += 1
+    #messagecount += 1
     if message.UnRead == True:
-        print(message.sender)
-        print(message.subject)
+       messagecount += 1
 
+print(messagecount)
+        #print(message.sender)
+        #print(message.subject)
+'''
         if 'absence' in message.subject:
             print("Found message with absence")
 
@@ -26,7 +39,9 @@ for message in messages:
             Msg.Subject = 'Got your ' + message.subject + ' email'
             Msg.HTMLBody = 'Hi' + str(message.sender) + "\n" + " sorry you are not well"
 
-            Msg.To = message.sender.GetExchangeUser().PrimarySmtAddress
-            Msg.ReadReceiptRequested = True
+'''
+
+            #Msg.To = message.sender.GetExchangeUser().PrimarySmtAddress
+            #Msg.ReadReceiptRequested = True
             
-            Msg.Send()
+            #Msg.Send()
